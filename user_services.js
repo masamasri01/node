f3cfee141536fd0c53108395ee3ddf5253538534
Masa Hassan Masri <93089580+masamasri01@users.noreply.github.com>
2023-05-06 18:50:41 +0300

    Add files via upload

---
+++ user_services.js
@@ -0,0 +1,56 @@
+
+const UserModel=require('../model/user_model')
+const jwt =  require('jsonwebtoken');
+class UserService{
+ static async registerMomUser(email,password){
+    try{
+        const createMomUser = new UserModel.MomModel({email,password});
+        return await createMomUser.save();
+
+    }catch(err){
+        throw err;
+    }
+}
+    static async registerTeacherUser(email,password){
+        try{
+       
+            const createUser = new UserModel.TeacherModel({email,password});
+            return await createUser.save();
+    
+        }catch(err){
+            throw err;
+        }
+ }
+ static async checkMomuser(email){
+    try {
+        return await UserModel.MomModel.findOne({email})
+    } catch (error) {
+        throw error
+    }
+}
+static async checkTeacheruser(email){
+    try {
+        return await UserModel.TeacherModel.findOne({email})
+    } catch (error) {
+        throw error
+    }
+}
+static async generateToken(tokenData, secretKey,JWT_EXPIRE){
+     return jwt.sign(tokenData , secretKey, { expiresIn: JWT_EXPIRE }  );
+}
+static async getMomUserByEmail(email){
+    try{
+        return await UserModel.MomModel.findOne({email});
+    }catch(err){
+        console.log(err);
+    }
+}
+static async getTeacherUserByEmail(email){
+    try{
+        return await UserModel.TeacherModel.findOne({email});
+    }catch(err){
+        console.log(err);
+    }
+}
+}
+module.exports = UserService;
